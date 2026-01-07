340da5e2d4275511ba2a549a75ecd874de72e4af
OfficialMabz <m.rahman180801@gmail.com>
2026-01-07 15:25:17 +0000

    configured aks

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,16 +8,3 @@ variable "acr_name" {
   type        = string
   default     = "mahbubrahmanacr"
 }
-
-# variable "tags" {
-#   description = "The default tags to associate with resources."
-#   type        = map(string)
-# }
-
-# variable "aks_config" {
-#   description = "AKS Configuration"
-# }
-
-# variable "app_gtw_config" {
-#   description = "Application Gateway Configuration"
-# }
